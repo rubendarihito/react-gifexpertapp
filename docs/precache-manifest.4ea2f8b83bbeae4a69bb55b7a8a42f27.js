887a3f3a5ba306c678286d764c37d35290cde5bc
Ruben Zambrano <ruben.1993.dario@hotmail.es>
2021-10-04 14:59:00 -0500

    docs agregado

---
+++ docs/precache-manifest.4ea2f8b83bbeae4a69bb55b7a8a42f27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c87e9e766608c1c88e007c3eafdf8a56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77318944c7374658101c",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77318944c7374658101c",
+    "url": "/static/js/main.303f12ae.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
